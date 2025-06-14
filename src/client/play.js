1baa7465dc1a33efb341ad1c1c5ee98aacc40f1f
ZZ <shadow1088punch@gmail.com>
2025-06-14 20:26:32 +0200

    started over again

--- src/client/play.js
+++ src/client/play.js
@@ -1,47 +1,101 @@
-const IMAGES = {
-  ship1: null,
-  ship2: null,
-  ship3: null,
-  ship4: null,
-};
+let WIDTH, HEIGHT;
+let canvas;
+
+let gameArea = {}; // this will now *be* the canvas
+let gridArea = {};
+let sideArea = {};
+let TILES = [];
 
 class Tile {
-  constructor(x, y) {
-    this.cords = new Cords(x, y); //pozice x a v rámci gridu
-    this.isHit = false; //true pokud je pole zasaženo
-    this.isShip = false; //true pokud je pole součástí lodi
-    this.isSank = false; //true pokud je pole součástí potopené lodi
-  }
-}
-
-class Ship {
-  constructor(owner, size) {
-    this.x = 0;
-    this.y = 0;
-    this.orientation = 1;
-    this.owner = currentPlayer;
-    this.size = this.col = this.row = this.image = "ship" + size;
-  }
-}
-
-// P5 function - "A function that's called once to load assets before the sketch runs."
-function preload() {
-  loadImages();
-}
-
-// loads all images into an object
-function loadImages() {
-  for (let i = 0; i < Object.keys(IMAGES).length; i++) {
-    img_name = Object.keys(IMAGES)[i];
-    IMAGES[img_name] = loadImage(`img/${img_name}.png`);
+  constructor() {
+    this.id = 0; // ship id (size), up tp 4, if bigger: id-5 = size;
+    this.state = 0; // 1: hit, 0: not hit
   }
 }
 
 function setup() {
-  createCanvas(1200, 500);
+  WIDTH = windowWidth;
+  HEIGHT = windowHeight;
+
+  updateLayout(); // calculate sizes before creating canvas
+
+  canvas = createCanvas(gameArea.w, gameArea.h);
+  canvas.position(gameArea.x, gameArea.y); // move canvas to proper location
 }
 
 function draw() {
-  imageMode(CENTER);
-  image(IMAGES.ship1, 40, 40, 80, 160);
+  background("purple"); // game area = white
+
+  // Grid area (left square)
+  fill(200); // light gray
+  noStroke();
+  rect(gridArea.x, gridArea.y, gridArea.w, gridArea.h);
+
+  // Side area (right rectangle)
+  fill(170); // slightly darker gray
+  noStroke();
+  rect(sideArea.x, sideArea.y, sideArea.w, sideArea.h);
+
+  drawGrid();
+}
+
+function windowResized() {
+  WIDTH = windowWidth;
+  HEIGHT = windowHeight;
+
+  updateLayout();
+  resizeCanvas(gameArea.w, gameArea.h);
+  canvas.position(gameArea.x, gameArea.y);
+}
+
+function updateLayout() {
+  // Canvas is centered and sized like gameArea
+  gameArea.w = (WIDTH / 6) * 4;
+  gameArea.h = (HEIGHT / 6) * 4;
+  gameArea.x = WIDTH / 6;
+  gameArea.y = HEIGHT / 6;
+
+  // Grid area (square on the left)
+  gridArea.h = gameArea.h;
+  gridArea.w = gameArea.h; // square
+  gridArea.x = 0; // inside canvas, so relative to (0, 0)
+  gridArea.y = 0;
+
+  // Side area (fills remaining horizontal space)
+  sideArea.x = gridArea.w;
+  sideArea.y = 0;
+  sideArea.w = gameArea.w - gridArea.w;
+  sideArea.h = gameArea.h;
+}
+
+function drawGrid() {
+  stroke("black");
+  let cols = 10;
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < cols; j++) {
+      TILES[j + cols * i] = new Tile();
+    }
+    line(
+      gridArea.x + (i * gridArea.w) / cols,
+      gridArea.y,
+      gridArea.x + (i * gridArea.w) / cols,
+      gridArea.y + gridArea.h,
+    );
+    line(
+      gridArea.x,
+      gridArea.y + (i * gridArea.h) / cols,
+      gridArea.x + gridArea.w,
+      gridArea.y + (i * gridArea.h) / cols,
+    );
+  }
+}
+
+function mouseClicked() {
+  tile = getTile(mouseX, mouseY);
+}
+
+function getTile(x, y) {
+  let col = floor((x / gridArea.w) * 10);
+  let row = floor((y / gridArea.h) * 10);
+  return [col, row];
 }
